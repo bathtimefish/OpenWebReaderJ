7a930f9a5ac608345f08643012a7d6f0fd0bd0c9
BathTimeFish <bathtimefish@gmail.com>
2011-06-12 18:11:09 +0900

    API Key認証のプロトタイプを追加

--- core.php
+++ core.php
@@ -1,5 +1,5 @@
 <?
-# Open Web Reader Core Test 0.01
+// Open Web Reader Core Test 0.01
 $SYSTEM_DIR = '/var/www/owr';
 $TMP_DIR = $SYSTEM_DIR . '/tmp';
 
@@ -7,35 +7,32 @@ $FileName = uniqid();
 
 $callbackId = $_GET['callback'];
 $text = urldecode($_GET['text']);
+$apiKey = $_GET['key'];
 
-# 1. phantomjs
-# $cmd = $SYSTEM_DIR . '/phantom.sh ' . $FileName;
-# $success = exec($cmd, $result);
+if( checkApiKey($apiKey) ) {
+    // 1. open_jtalk
+    $cmd = $SYSTEM_DIR . '/talk.sh "' . $text . '" ' . $FileName;
+    $success = exec($cmd, $result);
 
-# $fp = fopen($TMP_DIR.'/'.$FileName.'.txt', 'r');
-# $text = null;
-# while( !feof($fp) ){
-#   $text = fgetss($fp, 4096);
-# }
-# fclose($fp);
-# var_dump($text);
+    // 2. ffmpeg
+    $cmd = $SYSTEM_DIR . '/ffmpeg_enc.sh ' . $FileName;
+    $success = exec($cmd, $result);
 
-# 2. open_jtalk
-$cmd = $SYSTEM_DIR . '/talk.sh "' . $text . '" ' . $FileName;
-$success = exec($cmd, $result);
-
-# 3. ffmpeg
-$cmd = $SYSTEM_DIR . '/ffmpeg_enc.sh ' . $FileName;
-$success = exec($cmd, $result);
-
-# 4. JSONize
-$baseUrl = 'http://' . $_SERVER['HTTP_HOST'];
-$arrRes['audios'] = array(
-  'mp4' => $baseUrl.'/owr/audios/'.$FileName.'.mp4',
-  'ogg' => $baseUrl.'/owr/audios/'.$FileName.'.ogg'
-);
-$response = json_encode( $arrRes );
+    // 3. JSONize
+    $baseUrl = 'http://' . $_SERVER['HTTP_HOST'];
+    $arrRes['audios'] = array(
+			      'mp4' => $baseUrl.'/owr/audios/'.$FileName.'.mp4',
+			      'ogg' => $baseUrl.'/owr/audios/'.$FileName.'.ogg'
+			      );
+    $response = json_encode( $arrRes );
+}
 
 echo $callbackId . '(' . $response . ')';
+
+
+function checkApiKey($key) {
+    if (!empty($key) return true;
+    return false;
+}
 
 ?>
--- owrPlayer.js
+++ owrPlayer.js
@@ -2,7 +2,7 @@ $( function () {
 
   $(window).load( function() {
 
-    var apiUrl = location.protocol + '//' + location.hostname + '/owr/core.php?callback=?';
+    var apiUrl = location.protocol + '//' + location.hostname + '/owr/core.php?key=12345&callback=?';
     //var apiUrl = 'http://ec2-50-19-37-197.compute-1.amazonaws.com/owr/core.php?callback=?';
 
     var doc = $('#owr').text();
